8a71dcd884041f30cdeaef725885cbaa0150745e
Evelyn Rodriguez <evelynr812@gmail.com>
2022-02-17 18:37:35 -0500

    attempted post and delete

--- db.js
+++
@@ -1,5 +0,0 @@
-// module.exports = { 
-//     sequelize,
-//     Book,
-//     Genre
-// };
--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/library_books');
 const express = require('express');
 const app = express();
-//const db = require('./db');
+const methodOverride = require('method-override');
 
 const STRING = Sequelize.STRING;
 
 const Genre = sequelize.define('genre', {
     name: {
         type: STRING,
-        allowNull: false
+        allowNull: false,
     }
 });
 
@@ -22,6 +22,9 @@ const Book = sequelize.define('book', {
     title: {
         type: STRING,
         allowNull: false,
+        validate: {
+            notEmpty: true,
+        }
     },
     author: {
         type: STRING,
@@ -32,13 +35,42 @@ const Book = sequelize.define('book', {
 Book.belongsTo(Genre);
 Genre.hasMany(Book);
 
+app.use(methodOverride('_method'));
+
 app.get('/', (req, res) => res.redirect('books'));
 
-app.get('/books/', async(req, res, next) => {
+app.delete('/books/:id', async(req, res, next) => {
+    try {
+        const book = await Book.findByPk(req.params.id);
+        await book.destroy();
+        res.redirect(`/books/${book.genreId}`);
+    }
+    catch(err) {
+        next(err)
+    }
+});
+
+app.post('/books', async(req, res, next) => {
+    try {
+        const book = await Book.create(req.body);
+        res.redirect(`/books/${book.genreId}`);
+    }
+    catch(err) {
+        next(err)
+    }
+});
+
+app.get('/books', async(req, res, next) => {
     try {
         const books = await Book.findAll({
             include: [ Genre ]
         });
+
+        const genres = await Genre.findAll();
+
+        const options = genres.map( genre => {
+            return `<option value ='${genre.id}'>${genre.name}</option>`;
+        }).join('');
 
         const html = books.map( book => {
             return `
@@ -51,9 +83,23 @@ app.get('/books/', async(req, res, next) => {
         
         res.send(`
         <html>
+            <head>
+                <title> Friendly Neighborhood Library </title>
+            </head>
             <body>
-                <h1>Books</h1>
-                ${html}
+                <h1> Friendly Neighborhood Library </h1>
+                <h2> Fiction Section </h2>
+                <form method='POST'>
+                    <input name='title' placeholder='Book Title' />
+                    <input name='author' placeholder='Author' />
+                    <select name='genreId'> ${options} </select>
+                    <button> Return Book </button>
+                </form>
+                <div>
+                        <ul>
+                            ${html}
+                        </ul>
+                </div>
             </body>
         </html>
         `);
@@ -71,9 +117,14 @@ app.get('/genres/:id', async(req, res, next) => {
 
         const html = genre.books.map( book => {
             return `
+            <div>
                 <ul>
-                    ${book.title}
+                    ${book.title} 
+                    <form method='POST' action='/books/${book.id}?_method=delete'>
+                        <button> Check Out Book </button>
+                    </form>
                 </ul>
+            </div>
             `
         }).join('');
 
@@ -84,6 +135,7 @@ app.get('/genres/:id', async(req, res, next) => {
                     <div>
                         ${html}
                     </div>
+                    <button> <a href='/books'> Back </a> </button>
                 </body>
             </html>
         `);
